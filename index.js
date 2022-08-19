fd4e40116a7293169341da38b6929dd6c645e52a
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-08-20 01:56:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = [
+	{ year: "2015", result: "W" },
+	{ year: "2014", result: "N/A" },
+	{ year: "2013", result: "L" },
+];
+function superbowlWin() {
+	const WinObject = record.find((game) => game.result === "W");
+
+	console.log(`The year the team won was $(winObject.year)`);
+}
